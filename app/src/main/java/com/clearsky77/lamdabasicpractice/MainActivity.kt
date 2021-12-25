1841cd80f8199ba4f0dc36016fb81ef2c2898a06
clearsky77 <clearsky3264@gmail.com>
2021-12-25 23:38:08 +0900

    람다 함수 작성 / 변수 선언하지 않고 바로 매개변수로 입력  - Log.d 오타 정정

--- app/src/main/java/com/clearsky77/lamdabasicpractice/MainActivity.kt
+++ app/src/main/java/com/clearsky77/lamdabasicpractice/MainActivity.kt
@@ -17,16 +17,16 @@ class MainActivity : AppCompatActivity() {
         // 람다 함수
         Log.d("테스트"," ----- 람다 함수 -----")
         // fun a와 같은 기능을 람다 함수로
-//        val aa: (String)->Unit = { str: String -> Log.d("dd 내부","${str}한 람다 함수") }
-        val aa: (String)->Unit = { str -> Log.d("dd 내부","${str}한 람다 함수") }
-//        val aa = { str:String -> Log.d("dd 내부","${str}한 람다 함수") } // 얘는 안됨!!!
-        b(aa)
+//        val aa: (String)->Unit = { str: String -> Log.d("aa 내부","${str}한 람다 함수 aa") }
+        val aa: (String)->Unit = { str -> Log.d("aa 내부","${str}한 람다 함수 aa") }
+//        val aa = { str:String -> Log.d("aa 내부","${str}한 람다 함수") } // 얘는 안됨!!!
+        b({ str:String -> Log.d("aa 내부","${str}한 람다 함수 aa") }) // 이렇게는 가능하다
         c(aa)
     }
 
     // 고차 함수
     fun a (str: String){
-        Log.d("function a 내부","${str}한 함수 a")
+        Log.d("a 내부","${str}한 고차 함수 a")
     }
 
     fun b (method: (String)->Unit){
